fe621039923cc7c9e4df46d8a616776484b6c7b2
Jan0660 <58996212+Jan0660@users.noreply.github.com>
2020-08-06 11:16:51 +0200

    reee

--- Program.cs
+++ Program.cs
@@ -420,6 +420,7 @@ namespace AAAAAAA
             Console.Read();
         }
 
+        // im not foing to be writing those 400 lines of shit myself
         static void yeet(string ch)
         {
             Console.WriteLine("add_" + ch + ":;");
